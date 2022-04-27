dbb99d23d2bb991d5a7b071882182a2ebf5a4159
Sayli555 <96167212+Sayli555@users.noreply.github.com>
2022-04-27 22:44:04 +0530

    first commit

---
+++ src/Utils/Projects.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    img: "https://github.com/sahilshekh/project-photos/blob/master/photos/jio-mart.png?raw=true",
+    title: "JioMart Clone",
+    des: "This is the clone of JioMart website. A web application to buy Glocery and Daily-Need products. This is our team collaboration project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://jio-mart-clone.vercel.app/",
+    gitHub: "https://github.com/jibi966/JioMart-clone",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    img: "https://github.com/sahilshekh/project-photos/blob/master/photos/west-side.png?raw=true",
+    title: "Westside-ecommerce-Clone",
+    des: "This is the clone of Westside Shopping website , which is an e-commerce website of a Cloathings. This is our team collaboration project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://westside-ecommerce-clone.vercel.app/",
+    gitHub: "https://github.com/mdalishanali/Westside-ecommerce-Clone",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+     
+    ],
+  },
+  {
+    img: "https://github.com/sahilshekh/project-photos/blob/master/photos/purplle.png?raw=true",
+    title: "Purple-Clone",
+    des: "This is the clone of purple-skincare & makeup website. A web application to buy skincare and makeup products. This is solo project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://inspiring-aryabhata-5a6301.netlify.app/",
+    gitHub: "https://github.com/Sayli555/purple-clone",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    img: "https://miro.medium.com/max/1400/0*_a0kLpcnLpnrWlHB.png",
+    title: "Credo Clone",
+    des: "This is the clone of Credo website. A web application to buy skincare and makeup products. This is our team collaboration project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://westside-ecommerce-clone.vercel.app/",
+    gitHub: "https://github.com/Sayli555/credo-clone/",
+    technologies: ["HTML", "CSS", "JavaScript","NodeJS","ExpressJS","MongoDB"],
+  },
+ 
+];
