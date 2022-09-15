18a65b982f6512bd922a162265e25ff21292f327
Sayli Giri <sayligiri555@gmail.com>
2022-09-15 16:44:53 +0530

    link updated

--- src/Utils/Projects.js
+++ src/Utils/Projects.js
@@ -1,5 +1,25 @@
 export const projects = [
   {
+
+
+    img: "https://user-images.githubusercontent.com/97591028/180641416-56175994-d677-4ca7-9a16-f4063fa30ed0.png",
+    title: "HubSpot Clone",
+    des: "This is the clone of HubSpot website. HubSpot is a marketing , sales and customer service platform that helps companies grow better.. This is our team collaboration project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://leafy-moxie-ee6b4e.netlify.app/",
+    gitHub: "https://github.com/optimizershivam/Hubspot-clone",
+    technologies: ["React", "Redux", "Chakra UI","Javascript","CSS"],
+  },
+  {
+    img: "https://raw.githubusercontent.com/Sayli555/project-images/master/Screenshot%20(32).png",
+    title: "Beauty-Bebo Clone",
+    des: "This is the clone of Beauty-Bebo website. A web application to buy skincare and makeup products. This is solo project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://beutybebo-clone.vercel.app/",
+    gitHub: "https://github.com/Sayli555/beutybebo-clone",
+    technologies: ["React", "CSS", "JavaScript","NodeJS","ExpressJS","MongoDB"],
+  },
+  {
     img: "https://github.com/sahilshekh/project-photos/blob/master/photos/jio-mart.png?raw=true",
     title: "JioMart Clone",
     des: "This is the clone of JioMart website. A web application to buy Glocery and Daily-Need products. This is our team collaboration project at Full Stack-development bootcamp in Masai School.",
